ee652a820ab5e456e281b3f2aa63db2aa0560cf1
Pavel Petracek <petrapa6@fel.cvut.cz>
2022-02-28 18:58:00 +0100

    add links for datasets

--- subt_finals/download.sh
+++ subt_finals/download.sh
@@ -22,17 +22,16 @@ download_data() {
 # Add datasets here:
 echo Downloading data for dataset: DARPA Subterranean Challenge, Finals
 
-# TODO: add links
 URL=( "." "https://nasmrs.felk.cvut.cz/index.php/s/mf0K9r8rmBcAiav/download" # maps
-      "uav21_2" "https://nasmrs.felk.cvut.cz/index.php/s/5IuUWwMW1IzPZ9N/download"
-      "uav21_3" ""
-      "uav21_4" ""
-      "uav21_5" ""
-      "uav21_6" ""
-      "uav22_1" ""
-      "uav22_2" ""
-      "uav22_3" ""
-      "uav24_1" "" )
+      "uav21_2" "https://nasmrs.felk.cvut.cz/index.php/s/GNP4S6FWdj2FuSC/download"
+      "uav21_3" "https://nasmrs.felk.cvut.cz/index.php/s/bR5O4Z71QNeOsks/download"
+      "uav21_4" "https://nasmrs.felk.cvut.cz/index.php/s/WNAnezo38I99zpH/download"
+      "uav21_5" "https://nasmrs.felk.cvut.cz/index.php/s/4W7o12Uxb6VQwpy/download"
+      "uav21_6" "https://nasmrs.felk.cvut.cz/index.php/s/vRzfLKcNueDxAoH/download"
+      "uav22_1" "https://nasmrs.felk.cvut.cz/index.php/s/RaFXsset6szhWeR/download"
+      "uav22_2" "https://nasmrs.felk.cvut.cz/index.php/s/WEaEOFzQZcJN2vj/download"
+      "uav22_3" "https://nasmrs.felk.cvut.cz/index.php/s/LwfoPJ2gvCLqYpd/download"
+      "uav24_1" "https://nasmrs.felk.cvut.cz/index.php/s/Rbzuz6oFk6Vpwds/download" )
 
 ######################################################
 
